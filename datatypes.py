2fd9176addf0e17524f198ed79f51a0654a071fa
Yasiru Sampath <yasirusampath.dev@gmail.com>
2025-10-24 11:20:16 +0530

    Data Types in Python

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+#String 
+print("Hello My name is Yasiru Sampath")
+
+#integer
+print(345 + 233)
+print(234_445_656 + 234_345_454)
+
+#float
+print(254.42 + 2454.452)
+
+#boolean
+a = True
+b = False
+
+#complex 
+z = 3 + 5j
+print(type(z))
+
+#frozenset
+fs = frozenset({1, 2, 3})
+print(fs)
+
+coords = (5,30," Hello")
+print(coords)
+print(type(coords))
+
+snum = "123 "
+
+intnum = int(snum)
+print(intnum)
+print(type(intnum))
