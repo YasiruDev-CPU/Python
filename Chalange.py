6dfd80c86aa1bd2b410b66c0da782be6713da57f
Yasiru Sampath <yasirusampath.dev@gmail.com>
2025-10-25 08:17:10 +0530

    Calculate Two Digit Numbers

---
+++ Chalange.py
@@ -0,0 +1,8 @@
+userInput = input("Enter two digit numbers: ")
+
+firstDigit= userInput[0]
+secoundDigit = userInput[1]
+
+
+sumDigit = int(firstDigit) + int(secoundDigit)
+print("Two digit sum:",sumDigit)
